d33bce2b8d3000f9380a75b4c203601bc46cf0ed
murilo bastos <murilocraft5@gmail.com>
2025-03-22 15:44:12 -0300

    alura class

---
+++ app.js
@@ -0,0 +1,34 @@
+alert('bem vindo ao game');
+
+var name = 'lua';
+console.log(name);
+
+var idade = 18;
+console.log(idade);
+
+var numeroDeVendas = 50;
+console.log(numeroDeVendas);
+
+var saldoDisponivel = 1000;
+console.log(saldoDisponivel);
+
+alert('Erro! Preencha todos os campos');
+var mensagemDeErro = ('Erro! Preencha todos os campos');
+alert == mensagemDeErro;
+console.log(mensagemDeErro);
+
+var name = prompt('digite seu nome');
+console.log(name);
+
+var idade = prompt('digite sua idade');
+console.log(idade)
+
+if  (idade >= 18){
+    alert('Pode tirar a habilitação =)')
+}
+
+
+
+
+
+
