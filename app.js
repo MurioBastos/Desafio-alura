e899a21a50172a401fcce44bf48feb62036d1b14
murilo <murilocraft5@gmail.com>
2025-03-31 17:06:47 -0300

    desafio 3 alura uso de loop (while)

--- app.js
+++ app.js
@@ -1,32 +1,64 @@
-alert('bem vindo ao game');
+// alert('bem vindo ao game');
 
-var name = 'lua';
-console.log(name);
+// var name = 'lua';
+// console.log(name);
 
-var idade = 18;
-console.log(idade);
+// var idade = 18;
+// console.log(idade);
 
-var numeroDeVendas = 50;
-console.log(numeroDeVendas);
+// var numeroDeVendas = 50;
+// console.log(numeroDeVendas);
 
-var saldoDisponivel = 1000;
-console.log(saldoDisponivel);
+// var saldoDisponivel = 1000;
+// console.log(saldoDisponivel);
 
-alert('Erro! Preencha todos os campos');
-var mensagemDeErro = ('Erro! Preencha todos os campos');
-alert == mensagemDeErro;
-console.log(mensagemDeErro);
+// alert('Erro! Preencha todos os campos');
+// var mensagemDeErro = ('Erro! Preencha todos os campos');
+// alert == mensagemDeErro;
+// console.log(mensagemDeErro);
 
-var name = prompt('digite seu nome');
-console.log(name);
+// var name = prompt('digite seu nome');
+// console.log(name);
 
-var idade = prompt('digite sua idade');
-console.log(idade)
+// var idade = prompt('digite sua idade');
+// console.log(idade)
 
-if  (idade >= 18){
-    alert('Pode tirar a habilitação =)')
+// if  (idade >= 18){
+//     alert('Pode tirar a habilitação =)')
+// }
+
+let contador = 1; 
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
 }
 
+
+
+let contador2 = 10;
+while (contador2 >= 1){
+        console.log(contador2);
+        contador2--;
+}
+
+
+let numeroMaximo = 0; 
+let chute = prompt("digame um numero entre 10 e 0");
+while(chute >= numeroMaximo) {
+    console.log(chute);
+    chute--;
+
+}
+
+let numeroMaximo2 = prompt("digite um numero 0 e 15");
+let contador3 = 0;
+while (contador3 <= numeroMaximo2) {
+    console.log(contador3)
+    numeroMaximo2++;
+    
+
+    }
+    
 
 
 
